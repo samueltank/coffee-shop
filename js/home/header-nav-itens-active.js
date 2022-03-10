f3635942a26375a015d5c6d3c2b05d240fd23e21
Samuel Tank <dev.samueltank@gmail.com>
2022-03-10 10:29:32 -0300

    Add. movimento coffee bean navbar.

--- js/home/header-nav-itens-active.js
+++ js/home/header-nav-itens-active.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// para capturar a posição de uma elemento: .getBoundingClientRect();
+
 const navLinks = document.querySelectorAll(".nav-list-item > a");
 const bean     = document.querySelector("#coffee-bean-bar");
 
@@ -7,14 +9,24 @@ const bean     = document.querySelector("#coffee-bean-bar");
 function active() {
   navLinks.forEach((item) => {
     item.classList.remove("active");
-    this.classList.add("active");
-    // arrow func. não tem 
+    // arrow func. não recebe "this".
   }); 
+  this.classList.add("active");
+  this.appendChild(bean);  
 
-  // move bean:
-  (function() {
-    
-  })();
+  /* Testing for move bean: */
+
+  // const elementCoordenate = this.getBoundingClientRect().x;
+  // const beanCoordenate    = bean.getBoundingClientRect().x = 
+  // elementCoordenate;
+  // const beanReposition    = beanCoordenate;
+  // console.log(beanCoordenate)
+
+  // if (navLinks[1].classList.contains("active")) {
+  //   bean.style.transform = `translateX(${beanReposition}px)`;
+  // };
+
+  
 }
 
 navLinks.forEach((item) => {
