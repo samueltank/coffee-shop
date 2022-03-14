a61e8430af5154a00b1737ccbc11d02a4642b80a
Samuel Tank <dev.samueltank@gmail.com>
2022-03-14 11:39:37 -0300

    atualização de progresso

--- js/home/header_nav_itens_bean_move.js
+++ js/home/header_nav_itens_bean_move.js
@@ -4,36 +4,36 @@ const navLinks = document.querySelectorAll(".nav-list-item > a");
 const bean     = document.querySelector("#coffee-bean-bar");
 
 // função para aplicação de animação de levante ao click:
-function active() 
-{
+function active() {
   navLinks.forEach((item) => {
     item.classList.remove("active");
   }); 
   this.classList.add("active");
 
   // captura e armazenamento posição X e largura do elemento 'this':
-  let elementCoordenateX     = this.getBoundingClientRect().x;
-  let elementWidth           = this.getBoundingClientRect().width;
+  let elementCoordenateX = this.getBoundingClientRect().x;
+  let elementWidth       = this.getBoundingClientRect().width;
 
   // captura e armazenamento posição X e largura do elemento 'bean':
-  let beanCoordenateX        = bean.getBoundingClientRect().x;
-  let beanWidth              = bean.getBoundingClientRect().width;
+  let beanCoordenateX    = bean.getBoundingClientRect().x;
+  let beanWidth          = bean.getBoundingClientRect().width;
 
-  let elementThis = {
-    "AxisX"      : Math.round(elementCoordenateX),
-    "center"     : Math.round(elementWidth / 2)
+  // objetos com coordenadas necessárias para animação:
+  let elementThis = { // objeto elemento.
+    "AxisX" : Math.round(elementCoordenateX),
+    "center": Math.round(elementWidth / 2)
   };
-  let beanObject  = {
-    "AxisX"      : Math.round(beanCoordenateX),
-    "center"     : Math.round(beanWidth / 2)
+  let beanObject  = { // objeto 'bean'.
+    "AxisX" : Math.round(beanCoordenateX),
+    "center": Math.round(beanWidth / 2)
   };
 
   let move = (elementThis.AxisX - beanObject.AxisX) +
-  (elementThis.center - beanObject.center);
+    (elementThis.center - beanObject.center);
 
   console.log(elementThis);
   console.log(beanObject);
-  console.log(move);
+  console.log(move);  
 
   bean.style.transform += `translate(${move}px, 0px)`;
 }
