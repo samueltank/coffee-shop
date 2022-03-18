0996792da2664bb07e35b5e2cac9d2a35f47d09f
Samuel Tank <dev.samueltank@gmail.com>
2022-03-18 11:46:20 -0300

    snap

--- js/home/header_nav_scrolling.js
+++ js/home/header_nav_scrolling.js
@@ -1,7 +1,10 @@
 "use strict";
 
+/* script para determinar o estilo da barra quando a rolagem ocorrer */
 const header = document.querySelector(".container-sec-home-header");
+const logo   = document.querySelector(".header-sec-home-logo");
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 0);
+  logo.classList.toggle("dynami-logo", window.scrollY > 0);
 });
